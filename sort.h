fabb69fbfd0c5492af9db9099dd981d03c76a77e
baydre_africa <baydreafrica@gmail.com>
2023-09-25 19:23:27 +0100

    updated header file

--- sort.h
+++ sort.h
@@ -1,11 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-
-/**/
 #include <stdio.h>
 #include <stdlib.h>
-/**/
-void print_array(const int *array, size_t size);
 
 /**
  * struct listint_s - Doubly linked list node
@@ -21,7 +17,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**/
+/* print_array & print_list functions*/
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* sorting functions */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
